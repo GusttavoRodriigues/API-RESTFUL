32345f2a840d47e0209a88cfa0df43317f729acd
GusttavoRodriigues <102168608+GusttavoRodriigues@users.noreply.github.com>
2023-10-25 02:51:48 -0300

    Initial commit

---
+++ APICatalago/APICatalago/Models/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalago.Models;
+
+public class Categoria
+{
+
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();   
+    }
+    public int Id  { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string? ImagenUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
+
+}
